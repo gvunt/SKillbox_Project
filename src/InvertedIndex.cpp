cbc71c1dc6d0e96a784cf2d6b2f5fb692e700d9b
gvunt <128705651+gvunt@users.noreply.github.com>
2024-07-07 20:30:00 +0900

    Add files via upload

---
+++ src/InvertedIndex.cpp
@@ -0,0 +1,20 @@
+#include "../include/InvertedIndex.h"
+#include <sstream>
+#include <algorithm>
+
+void InvertedIndex::addDocument(const std::string& document, int docId) {
+    std::istringstream iss(document);
+    std::string word;
+    std::lock_guard<std::mutex> lock(indexMutex);
+    while (iss >> word) {
+        std::transform(word.begin(), word.end(), word.begin(), ::tolower);
+        index[word].insert(docId);
+    }
+}
+
+const std::unordered_set<int>& InvertedIndex::getDocuments(const std::string& word) const {
+    static std::unordered_set<int> emptySet;
+    std::lock_guard<std::mutex> lock(indexMutex);
+    auto it = index.find(word);
+    return (it != index.end()) ? it->second : emptySet;
+}
